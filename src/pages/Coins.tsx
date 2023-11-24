e7ca4eddbf604565637c51e8f137e7e1e421f08f
kimsuji <skme1103@naver.com>
2023-11-24 09:14:24 +0900

    231124_코인앱_라우터연결테스트

--- src/pages/Coins.tsx
+++ src/pages/Coins.tsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Coins() {
-    return <h1>Coins</h1>
+    return (
+        <div>
+          <h1>Coins</h1>
+          <p>가장 먼저 보여지는 페이지입니다.</p>
+          <Link to="/coin">코인페이지</Link>
+        </div>
+      );
 }
 
 export default Coins;
