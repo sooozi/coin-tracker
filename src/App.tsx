03b9ba97f5f926b316de7fa8a4d93074daf4598b
kimsuji <skme1103@naver.com>
2023-11-24 09:04:10 +0900

    231124_코인앱_라우터연결테스트

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hi! soozi! Edit <code>src/App.tsx</code> and save to reload.
+          라우터를 연결해보자!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
