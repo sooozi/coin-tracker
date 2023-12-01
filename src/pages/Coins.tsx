8a0e3ec183b8ffee11b27f69c7733e5432bbd182
kimsuji <skme1103@naver.com>
2023-12-01 21:56:09 +0900

    231201_코인 앱_탭 드디어 해결

--- src/pages/Coins.tsx
+++ src/pages/Coins.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
@@ -83,14 +83,14 @@ function Coins() {
   useQuery('allCoins', fetchCoins);
   const [coins, setCoins] = useState<CoinInterface[]>([]);
   const [loading, setLoading] = useState(true);
-  useEffect(() => {
-      (async() => {
-          const response = await fetch("https://api.coinpaprika.com/v1/coins");
-          const json = await response.json();
-          setCoins(json.slice(0, 50));
-          setLoading(false);
-      })()
-  }, []);
+  // useEffect(() => {
+  //     (async() => {
+  //         const response = await fetch("https://api.coinpaprika.com/v1/coins");
+  //         const json = await response.json();
+  //         setCoins(json.slice(0, 50));
+  //         setLoading(false);
+  //     })()
+  // }, []);
 
   return (
     <Container>
