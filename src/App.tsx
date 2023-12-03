4b45a9808de95f1618b45479602ca0c6c2bd10e4
kimsuji <skme1103@naver.com>
2023-12-03 18:36:31 +0900

    231203_코인 앱_리액트 쿼리 오류 수정 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { ReactQueryDevtools } from 'react-query/devtools'; // 수정된 부분
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'; // 수정된 부분
 import { ThemeProvider } from 'styled-components';
 import AppRouter from './AppRouter';
 import GlobalStyle from './style/GlobalStyle';
