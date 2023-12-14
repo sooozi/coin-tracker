c23a7307b2721a288a9296517ae06542bd098e87
kimsuji <skme1103@naver.com>
2023-12-14 19:38:04 +0900

    231214_코인앱_차트만들기

--- src/pages/Chart.tsx
+++ src/pages/Chart.tsx
@@ -5,23 +5,27 @@ import { fetchCoinHistory } from "../api";
 interface ChartPropsData {
   coinId : string
 }
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
 function Chart() {
   const coinId =useOutletContext();
-  const { isLoading : ChartPropsLoading, data : ChartPropsData } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical>(["ohlcv", coinId], () =>
     fetchCoinHistory(`${coinId}`)
   );
 
-  const loading = ChartPropsLoading;
 
   return (
-    <>
-      {loading ? (
-              <>Loading...💤</>
-            ) : (
-              <>Charttt${coinId}</>
-            )
-      } 
-    </>
+        <>Charttt${coinId}</>
+      
   );
 }
 
