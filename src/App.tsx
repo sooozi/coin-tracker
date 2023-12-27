819e2a4d6e3e93a54c654e19de89da3881158653
kimsuji <skme1103@naver.com>
2023-12-27 20:12:43 +0900

    231227_코인앱_다크모드, 라이트모드

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from 'react-query/devtools'; // 수정된 부분
+import { useState } from "react";
 import { HelmetProvider } from "react-helmet-async";
 import { ThemeProvider } from 'styled-components';
 import AppRouter from './AppRouter';
 import GlobalStyle from './style/GlobalStyle';
-import { theme } from './theme';
-
-// const queryClient = new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       refetchOnWindowFocus: false,
-//     },
-//   },
-// });
+import { darkTheme, lightTheme } from './theme';
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
+  const toggleDark = () => setIsDark((current) => !current);
+
   return ( 
     <div>
-      {/* <QueryClientProvider client={queryClient}> */}
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+          <button onClick={toggleDark}>Toggle Mode</button>
           <GlobalStyle />
-          < HelmetProvider >
-          <AppRouter />
+          <HelmetProvider >
+            <AppRouter />
           </HelmetProvider>
-          {/* <ReactQueryDevtools initialIsOpen={true} /> */}
         </ThemeProvider>
-      {/* </QueryClientProvider> */}
     </div>
   );
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,20 @@
 import { DefaultTheme } from "styled-components";
 
-export const theme: DefaultTheme = {
+export const darkTheme: DefaultTheme = {
   bgColor: "#1c1b25",
   contBgColor: "#2c2a3a",
   textColor: "rgba(251, 251, 251, 0.5)",
   // textColor: "rgb(42, 42, 44, 0.5)",
   pointColor: "rgb(99, 89, 236)",
+  accentColor: "rgb(251, 251, 251)",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "whitesmoke",
+  contBgColor: "#2c2a3a",
+  textColor: "black",
+  // textColor: "rgb(42, 42, 44, 0.5)",
+  pointColor: "#2c2a3a",
   accentColor: "rgb(251, 251, 251)",
 };
 
